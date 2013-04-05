17b3af136fdd4a0b6bbb663dd623fc0ec5c2305a
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-04-05 16:17:32 +0200

    amdOneFile should resolve relative require() calls

--- test/output/amdOneFile.js
+++ test/output/amdOneFile.js
@@ -1,18 +1,18 @@
-define('up', function(require, module, exports, global) {
-    require("./e")
-    var a = require("./up1").name
-    var b = new (require("./up1"))()
-    var c = new require("./up1")
-    var d = {a: require("./up1")}
-    var e = require("./e") + require("./e")
-    module.exports = require("./up1")()
+define('output/up', function(require, module, exports, global) {
+    require("output/e")
+    var a = require("output/up1").name
+    var b = new (require("output/up1"))()
+    var c = new require("output/up1")
+    var d = {a: require("output/up1")}
+    var e = require("output/e") + require("output/e")
+    module.exports = require("output/up1")()
 });
 
-define('e', function(require, module, exports, global) {
+define('output/e', function(require, module, exports, global) {
     module.exports = 'e'
 });
 
-define('up1', function(require, module, exports, global) {
+define('output/up1', function(require, module, exports, global) {
     module.exports = function() {
         console.log("up1")
     }
