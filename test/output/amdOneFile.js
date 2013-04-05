ea952788138e3f565e0cdc5e5db00ce4a1baa526
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-04-05 18:24:11 +0200

    Update amdOneFile test output with correct module IDs.

--- test/output/amdOneFile.js
+++ test/output/amdOneFile.js
@@ -1,18 +1,18 @@
-define('output/up', function(require, module, exports, global) {
-    require("output/e")
-    var a = require("output/up1").name
-    var b = new (require("output/up1"))()
-    var c = new require("output/up1")
-    var d = {a: require("output/up1")}
-    var e = require("output/e") + require("output/e")
-    module.exports = require("output/up1")()
+define('test/fixtures/up', function(require, module, exports, global) {
+    require("test/fixtures/e")
+    var a = require("test/fixtures/up1").name
+    var b = new (require("test/fixtures/up1"))()
+    var c = new require("test/fixtures/up1")
+    var d = {a: require("test/fixtures/up1")}
+    var e = require("test/fixtures/e") + require("test/fixtures/e")
+    module.exports = require("test/fixtures/up1")()
 });
 
-define('output/e', function(require, module, exports, global) {
+define('test/fixtures/e', function(require, module, exports, global) {
     module.exports = 'e'
 });
 
-define('output/up1', function(require, module, exports, global) {
+define('test/fixtures/up1', function(require, module, exports, global) {
     module.exports = function() {
         console.log("up1")
     }
