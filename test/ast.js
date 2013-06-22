ef44be6adacf7a8edbda0a7dfbf1eda99ea18299
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-06-22 14:29:36 +0200

    Show string diff when comparing the AST JSON structures fails

--- test/ast.js
+++ test/ast.js
@@ -4,6 +4,7 @@ var assert = require('assert')
 var fs     = require('fs')
 var wrapup = require('../lib/main')
 var test   = require('./run')
+var diff   = require('ansidiff')
 
 var wrup = wrapup()
 
@@ -17,9 +18,14 @@ wrup.on("error", function(err){
 
 wrup.require(__dirname + "/fixtures/up").up(function(err, actual){
     assert.ifError(err)
-    var should = fs.readFileSync(__dirname + "/output/ast.json")
+    var should = fs.readFileSync(__dirname + "/output/ast.json", "utf-8")
     fs.writeFileSync(__dirname + "/output/ast.result.json", actual)
-    assert.equal(actual, should, "AST JSON structure should be equal")
+    try {
+        assert.equal(actual, should, "AST JSON structure should be equal")
+    } catch (e){
+        console.log(diff.words(should, actual));
+        throw e;
+    }
     test.passed('ast')
 })
 
