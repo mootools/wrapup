38736744ed35541a5aa0a403ca97ac0ede452d92
Arian Stolwijk <stolwijk.arian@gmail.com>
2013-07-23 20:54:21 +0200

    Move the Out of Scope error to a better place.

--- test/graph.js
+++ test/graph.js
@@ -3,6 +3,7 @@
 var fs     = require('fs')
 var assert = require('assert')
 var passed = require('./run').passed
+var diff   = require('ansidiff')
 
 var wrup = require('../lib/main')({
     graph: true
@@ -10,7 +11,12 @@ var wrup = require('../lib/main')({
 
 wrup.require(__dirname + '/fixtures/b').up(function(err, actual){
     assert.ifError(err)
-    var should = fs.readFileSync(__dirname + '/output/graph.dot')
-    assert.equal(should, actual, "the generated dot should be equal")
+    var should = fs.readFileSync(__dirname + '/output/graph.dot', 'utf-8')
+    try {
+        assert.equal(should, actual, "the generated dot should be equal")
+    } catch (e){
+        console.log(diff.words(should, actual));
+        throw e
+    }
     passed("graph")
 })
--- test/unit/output/ascii.js
+++ test/unit/output/ascii.js
@@ -4,19 +4,18 @@ var expect = require('expect.js')
 
 var Storage = require('../../../lib/moduleStorage')
 var Output = require('../../../lib/output/ascii')
-
+var Module = require('../../../lib/module')
 
 describe('output/ASCII', function(){
 
     it('should format the required modules and dependencies as text', function(done){
 
         var storage = new Storage()
-        storage.put('a', {
-            deps: [{full: 'b'}, {full: 'c'}, {full: 'd'}]
-        }).put('e', {
-            namespace: 'eee',
-            deps: [{full: 'f'}, {full: 'g'}]
-        })
+        var a = new Module('a'), e = new Module('e')
+        a.deps = [new Module('b'), new Module('c'), new Module('d')]
+        e.namespace = 'eee'
+        e.deps = [new Module('f'), new Module('g')]
+        storage.put('a', a).put('e', e)
 
         var output = new Output()
         output.withStorage(storage)
