165847ff2d35d60e72d28451ae8e732610582e83
Cristian Carlesso <kentaromiura@gmail.com>
2014-01-08 12:09:52 +0000

    Update browser-wrapper.js
    using true global object

--- includes/browser-wrapper.js
+++ includes/browser-wrapper.js
@@ -1,11 +1,11 @@
-(function(modules){
+(function(modules, global){
     var cache = {}, require = function(id){
         var module = cache[id]
         if (!module){
             module = cache[id] = {}
             var exports = module.exports = {}
-            modules[id].call(exports, require, module, exports, typeof window == 'undefined' ? {} : window)
+            modules[id].call(exports, require, module, exports, global)
         }
         return module.exports
     }
-})({})
+})({}, this)
